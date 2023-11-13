42971bc8619572e1f7ab4c36308cefb61ed1ec63
jose <jose.camacho@skysite.com.ec>
2023-11-12 19:05:30 -0500

    modificacion tests

--- test_prueba.py
+++ test_prueba.py
@@ -4,16 +4,16 @@ from utilidades.facade_reserva import FachadaReserva
 
 class TestMiModulo(unittest.TestCase):
 
-    def turno_es_horario_laboral(self):
+    def test_turno_es_horario_laboral(self):
         self.assertEqual(FachadaReserva.turno_horario_laboral(fecha='2023-11-13', hora='10:00' ),  True)
 
-    def turno_no_es_horario_laboral(self):
+    def test_turno_no_es_horario_laboral(self):
         self.assertEqual(FachadaReserva.turno_horario_laboral(fecha='2023-11-13', hora='10:00' ),  False)
 
-    def fecha_es_feriado(self):
+    def test_fecha_es_feriado(self):
         self.assertEqual(FachadaReserva.es_feriado(fecha='2023-11-03'),  True)
     
-    def fecha_no_es_feriado(self):
+    def test_fecha_no_es_feriado(self):
         self.assertEqual(FachadaReserva.es_feriado(fecha='2023-11-13'),  False)
 
 
